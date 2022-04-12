38d8edbc147f3a9c528e5c806ce1265c9b931121
Bryant <3314tony@gmail.com>
2022-04-13 02:19:06 +0800

    first commit

---
+++ 220323 youtube/playList.js
@@ -0,0 +1,13 @@
+//youtube video id
+let playList = [
+    "SwZMS-ISbH4",
+    "ZBFlFACQL6E",//flash
+    "tK9HSZdf8kQ"//husky
+];
+
+//播放起訖秒數 
+let playTime = [
+    [101,105], 
+    [267,270], 
+    [180,192]
+];
